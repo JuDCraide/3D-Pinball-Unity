0317b88d77aa302397c4af8a57a83956241043f9
JúDCraide <juliadcraide31@gmail.com>
2023-07-09 10:00:55 -0300

    Menu e Game over

---
+++ Assets/Scripts/GerenteJogo.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerenteJogo : MonoBehaviour
+{
+
+    public int objectsCollided = 0;
+    public int points = 0;
+
+    public TMPro.TextMeshProUGUI TextPoints;
+    public TMPro.TextMeshProUGUI TextBumpersHit;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CollidedNewObject(){        
+        objectsCollided++;
+        //Debug.Log("Collided " + objectsCollided);
+        TextBumpersHit.SetText("Bumpers hit: " + objectsCollided);
+    }
+
+    public void AddPoints(int points ){        
+        this.points += points;
+        Debug.Log("Points " + this.points);
+        TextPoints.SetText("Points: " + this.points);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,44 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
+
 public class Player : MonoBehaviour {
     public float speed = 10.0f;
     Rigidbody rb;
 
     public int objectsCollided = 0;
-
     public int points = 0;
 
+    public float yKillZone = -5;
+
+    public TMPro.TextMeshProUGUI TextPoints;
+    public TMPro.TextMeshProUGUI TextBumpersHit;
+
     void Start() {
+        DontDestroyOnLoad(this.gameObject);
         rb = GetComponent<Rigidbody>();
+
+        TextBumpersHit.SetText("Bumpers hit: " + objectsCollided);
+        TextPoints.SetText("Points: " + points);
     }
 
     void FixedUpdate() {
+        if(this.gameObject.transform.position.y < yKillZone ){
+            SceneManager.LoadScene("Fim");
+        }
+
         float hMove = Input.GetAxis("Horizontal");
         float vMove = Input.GetAxis("Vertical");
         Vector3 moveDirection = new Vector3(hMove, 0, vMove);
 
         rb.AddForce(moveDirection * speed);
-
     }
 
     public void CollidedNewObject(){        
         objectsCollided++;
-        Debug.Log("Collided " + objectsCollided);
+        //Debug.Log("Collided " + objectsCollided);
+        TextBumpersHit.SetText("Bumpers hit: " + objectsCollided);
     }
 
     public void AddPoints(int points ){        
         this.points += points;
         Debug.Log("Points " + this.points);
+        TextPoints.SetText("Points: " + this.points);
     }
-    
-    // private void OnCollisionEnter(Collision collision) {
-    //     GameObject otherObject = collision.gameObject;
-
-    //     Debug.Log(otherObject.name);
-    //     if(otherObject.tag == "ChangeColor") {
-    //         otherObject.GetComponent<MeshRenderer>().material.color = Color.red;
-    //     }
-        
-    // }
 }
