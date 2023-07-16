625ef8427565f3408ec26c0244662a03f57c67a0
JúDCraide <juliadcraide31@gmail.com>
2023-07-16 00:37:13 -0300

    Add different messages to the end screen

--- Assets/Scripts/GerenteFim.cs
+++ Assets/Scripts/GerenteFim.cs
@@ -9,16 +9,26 @@ public class GerenteFim : MonoBehaviour
     public TMPro.TextMeshProUGUI TextPoints;
     public TMPro.TextMeshProUGUI TextBumpersHit;
     public TMPro.TextMeshProUGUI TextTempo;
+    public TMPro.TextMeshProUGUI TextEndMessage;
 
     GerenteJogo gj;
     // Start is called before the first frame update
     void Start()
     {
         gj = GameObject.Find("GerenteJogo").GetComponent<GerenteJogo>();
+
+        var bumpers = gj.objectsCollided;
+        var points  = gj.points;
         
-        TextBumpersHit.SetText("Bumpers hit: " + gj.objectsCollided + " / 12");
-        TextPoints.SetText("Points from collectables: " + gj.points + " / 2222");
-        Debug.Log("Timer: " + gj.minutos.ToString("00") + ":" + gj.segundos.ToString("00"));
+        if(bumpers == 12 && points == 2222){
+            TextEndMessage.SetText("Congrats!\nYou finish all the objectives");
+        } else {
+            TextEndMessage.SetText("Game Over!\nTry again to get all objectives");
+        }
+
+        TextBumpersHit.SetText("Bumpers hit: " + bumpers + " / 12");
+        TextPoints.SetText("Points from collectables: " + points + " / 2222");
+        //Debug.Log("Timer: " + gj.minutos.ToString("00") + ":" + gj.segundos.ToString("00"));
         TextTempo.SetText("Timer: " + gj.minutos.ToString("00") + ":" + gj.segundos.ToString("00"));
     }
 
